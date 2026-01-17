217c34cc188029245f1c7360c1b81259d0bd0a65
jpagcal <jpagcal_@outlook.com>
2026-01-16 17:03:45 -0700

    initial scaffold

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ wd-favorites
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ wd-favorites --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
